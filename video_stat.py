5189249fa918bc8abcbc7869254b5f497c64c6b8
Mac <amyatmon5566@gmail.com>
2026-01-04 10:46:54 +0630

    Added .env to .ignore and saved API in the env file

---
+++ video_stat.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY")
+
+
+
+Channel_Name = "MrBeast"
+
+def get_playlist_id():
+
+    try: 
+
+        URL =  f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={Channel_Name}&key={API_KEY}"
+        response = requests.get(URL)
+        data = response.json()
+
+        # print(json.dumps(data,indent=4))
+
+        channel_items=data["items"][0]
+
+        channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+        print(channel_playlistId)
+        return channel_playlistId
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == "__main__":
+        get_playlist_id()
